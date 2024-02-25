52dbc345334b8d4358612260459cbfa4b513e908
nonek <adambimek@gmail.com>
2024-02-25 10:56:44 +0100

    Initial commit

---
+++ src/main/java/czescjestemadas/kmduels/Duels.java
@@ -0,0 +1,18 @@
+package czescjestemadas.kmduels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Duels extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        
+    }
+
+    @Override
+    public void onDisable()
+    {
+        
+    }
+}
