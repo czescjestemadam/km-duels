6ccfdcc6ae06748f717d6291d030c8d848a1a047
nonek <adambimek@gmail.com>
2024-03-01 06:35:17 +0100

    item builder

---
+++ src/main/java/czescjestemadas/kmduels/utils/ItemBuilder.java
@@ -0,0 +1,49 @@
+package czescjestemadas.kmduels.utils;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.function.Consumer;
+
+public class ItemBuilder
+{
+	private final ItemStack item;
+
+	public ItemBuilder(ItemStack item)
+	{
+		this.item = item;
+	}
+
+	public ItemBuilder(Material material)
+	{
+		item = new ItemStack(material);
+	}
+
+	public ItemBuilder displayname(Component component)
+	{
+		item.editMeta(meta -> meta.displayName(component));
+		return this;
+	}
+
+	public ItemBuilder meta(Consumer<ItemMeta> func)
+	{
+		item.editMeta(func);
+		return this;
+	}
+
+	public ItemStack build()
+	{
+		return item.clone();
+	}
+
+
+	@Override
+	public String toString()
+	{
+		return "ItemBuilder{" +
+				"item=" + item +
+				'}';
+	}
+}
