0b15dff36eddcad45000cffb453f72933e87316d
nonek <adambimek@gmail.com>
2024-03-01 04:47:23 +0100

    gui config

--- src/main/java/czescjestemadas/kmduels/config/GuiConfig.java
+++ src/main/java/czescjestemadas/kmduels/config/GuiConfig.java
@@ -1,42 +1,44 @@
 package czescjestemadas.kmduels.config;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.configuration.ConfigurationSection;
 
-import java.util.HashMap;
-import java.util.Map;
+import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 
 public class GuiConfig implements Config
 {
-	public final Map<String, Integer> hotbarSlots = new HashMap<>();
-
-	public final Selector selectorKit = new Selector();
-	public final Selector selectorMap = new Selector();
-	public final Selector selectorFight = new Selector();
-
+	public Selector selectorsRankedFight;
+	public Selector selectorsKitEditor;
+	public Selector selectorsSpectate;
+	public Selector selectorsPartyFight;
+	public Selector selectorsPartySettings;
 
 	@Override
 	public void load(ConfigurationSection cfg)
 	{
-		final ConfigurationSection cfgHotbarSlots = cfg.getConfigurationSection("hotbar-slots");
-		for (String key : cfgHotbarSlots.getKeys(false))
-			hotbarSlots.put(key, cfgHotbarSlots.getInt(key));
-
-		selectorKit.load(cfg.getConfigurationSection("selector.kit"));
-		selectorMap.load(cfg.getConfigurationSection("selector.map"));
-		selectorFight.load(cfg.getConfigurationSection("selector.fight"));
+		final ConfigurationSection selectors = cfg.getConfigurationSection("selectors");
+		selectorsRankedFight = Selector.load(selectors.getConfigurationSection("ranked-fight"));
+		selectorsKitEditor = Selector.load(selectors.getConfigurationSection("kit-editor"));
+		selectorsSpectate = Selector.load(selectors.getConfigurationSection("spectate"));
+		selectorsPartyFight = Selector.load(selectors.getConfigurationSection("party-fight"));
+		selectorsPartySettings = Selector.load(selectors.getConfigurationSection("party-settings"));
 	}
 
+	
 	public static class Selector
 	{
 		public int size;
 		public Component title;
 
-		public void load(ConfigurationSection cfg)
+		public Selector(int size, Component title)
 		{
-			size = cfg.getInt("size");
-			title = MiniMessage.miniMessage().deserialize(cfg.getString("title"));
+			this.size = size;
+			this.title = title;
+		}
+
+		private static Selector load(ConfigurationSection cfg)
+		{
+			return new Selector(cfg.getInt("size"), miniMessage().deserialize(cfg.getString("title")));
 		}
 	}
 }
