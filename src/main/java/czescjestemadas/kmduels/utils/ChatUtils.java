6b8dfea97f80a365b77149030cc7376f37395589
nonek <adambimek@gmail.com>
2024-03-10 00:12:00 +0100

    party resolver

--- src/main/java/czescjestemadas/kmduels/utils/ChatUtils.java
+++ src/main/java/czescjestemadas/kmduels/utils/ChatUtils.java
@@ -3,11 +3,14 @@ package czescjestemadas.kmduels.utils;
 import czescjestemadas.kmduels.Duels;
 import czescjestemadas.kmduels.kits.DuelKit;
 import czescjestemadas.kmduels.maps.DuelMap;
+import czescjestemadas.kmduels.party.DuelParty;
+import czescjestemadas.kmduels.players.DuelPlayer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,6 +30,11 @@ public class ChatUtils
 	public static Component mmMap(String str, DuelMap map)
 	{
 		return mm(str, mapResolver(map));
+	}
+
+	public static Component mmParty(String str, DuelParty party)
+	{
+		return mm(str, partyResolver(party));
 	}
 
 
@@ -64,6 +72,17 @@ public class ChatUtils
 				Placeholder.unparsed("map_spawn_positions", spawnPos + ""),
 				Placeholder.unparsed("map_last_spawn_position", spawnPos.isEmpty() ? "null" : spawnPos.get(spawnPos.size() - 1).toString()),
 				Placeholder.unparsed("map_icon", map.getIcon() + "")
+		);
+	}
+
+	public static TagResolver partyResolver(DuelParty party)
+	{
+		final String members = String.join(", ", party.getPlayers().stream().map(DuelPlayer::getPlayer).map(Player::getName).toList());
+		return TagResolver.resolver(
+				Placeholder.unparsed("party_owner", party.getOwner().getPlayer().getName()),
+				Placeholder.unparsed("party_membercount", party.getPlayers().size() + ""),
+				Placeholder.unparsed("party_members", members),
+				Placeholder.unparsed("party_name", party.getName())
 		);
 	}
 }
